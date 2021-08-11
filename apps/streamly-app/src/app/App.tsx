8dd8d9df03e4d8cf465ac098eaedb01a2bf58e5e
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-10 21:09:16 -0300

    created component EventPage

--- apps/streamly-app/src/app/App.tsx
+++ apps/streamly-app/src/app/App.tsx
@@ -1,17 +1,31 @@
+import { FC, lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 
 import { theme } from './theme';
+import { ParticipantsContextProvider } from './contexts';
 
-export function App() {
+import { ErrorBoundary, ErrorFallback, SuspenseFallback } from './components';
+import Error404Page from './pages/Error404';
+
+const EventPage = lazy(() => import('./pages/Event'));
+
+export const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Switch>
-        <Route path="/" exact render={() => <div />} />
-      </Switch>
+      <ErrorBoundary fallback={<ErrorFallback />}>
+        <ParticipantsContextProvider>
+          <Suspense fallback={<SuspenseFallback />}>
+            <Switch>
+              <Route path="/events/:eventId" exact component={EventPage} />
+              <Route component={Error404Page} />
+            </Switch>
+          </Suspense>
+        </ParticipantsContextProvider>
+      </ErrorBoundary>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ apps/streamly-app/src/app/pages/Event.tsx
@@ -0,0 +1,78 @@
+import { FC, MutableRefObject, useCallback, useEffect, useRef } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import {
+  makeStyles,
+  Box,
+  Container,
+  Grid,
+  Backdrop,
+  CircularProgress,
+} from '@material-ui/core';
+
+import { useUserMedia, usePeerData } from '../hooks';
+import { Video, UserMediaControls } from '../components';
+
+type EventPageProps = unknown & RouteComponentProps<{ eventId: string }>;
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.modal,
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    paddingTop: theme.spacing(4),
+  },
+}));
+
+const EventPage: FC<EventPageProps> = ({ history, match }) => {
+  const videoRef =
+    useRef<HTMLVideoElement>() as MutableRefObject<HTMLVideoElement>;
+
+  const { mediaStream } = useUserMedia(videoRef);
+
+  const { participants } = usePeerData(match.params.eventId, mediaStream);
+
+  const handleClose = useCallback(() => {
+    history.push('/');
+  }, [history]);
+
+  const styles = useStyles();
+
+  return (
+    <>
+      <Backdrop className={styles.backdrop} open={false}>
+        <CircularProgress />
+      </Backdrop>
+      <Box className={styles.wrapper}>
+        <Box flexGrow={1}>
+          <Container>
+            <Grid
+              container
+              alignContent="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item xs={6} md={4}>
+                <Video ref={videoRef} />
+              </Grid>
+              {participants.map((participant) => (
+                <Grid key={participant.id} item xs={6} md={4}>
+                  <Video ref={participant.videoRef} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Box>
+
+        <UserMediaControls
+          mediaStream={mediaStream}
+          handleClose={handleClose}
+        />
+      </Box>
+    </>
+  );
+};
+
+export default EventPage;
