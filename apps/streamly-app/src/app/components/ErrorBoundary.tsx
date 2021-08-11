e7c536bbe29c98f3d3662c01d35d17dd83dab4ab
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-10 21:07:44 -0300

    created component ErrorBoundary

---
+++ apps/streamly-app/src/app/components/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import { Component, ReactChild } from 'react';
+
+type ErrorBoundaryProps = {
+  fallback: ReactChild;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: unknown) {
+    console.debug(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
