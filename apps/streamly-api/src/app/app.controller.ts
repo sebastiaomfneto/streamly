485d9d6ec086cf88b0642a39eaef21c46f4b8ae2
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-08-11 23:41:14 -0300

    added swagger decorators to AppController

--- apps/streamly-api/src/app/app.controller.ts
+++ apps/streamly-api/src/app/app.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   @Get('/uptime')
+  @ApiOkResponse()
   getUptime(): number {
     return process.uptime();
   }
